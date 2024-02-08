13f867e8e3361417af8776a5162c6ca43cdddae9
ilsantos <igor.lourencosantos@gmail.com>
2024-02-08 14:32:44 -0300

    Iniciando com a interface - Predicate.

---
+++ src/main/java/interfaces/funcional/predicate/InterfacePredicate.java
@@ -0,0 +1,12 @@
+package interfaces.funcional.predicate;
+
+public class InterfacePredicate {
+
+	public static void main(String[] args) {
+
+
+	System.out.println("Iniciando com a interface Predicate.");
+
+	}
+
+}
