075f4b4bcb6a1c1d8ca9aa6edd4ddb51224b4007
ilsantos <igor.lourencosantos@gmail.com>
2024-02-09 14:40:49 -0300

    Usando expressao lambda inline com a interface - Predicate.

--- src/main/java/interfaces/funcional/predicate/InterfacePredicate.java
+++ src/main/java/interfaces/funcional/predicate/InterfacePredicate.java
@@ -116,6 +116,45 @@ public class InterfacePredicate {
 		for (Produto p : list) {
 			System.out.println(p);
 		}
+		
+//		===============================================================
+		
+		list.add(new Produto("TV", 900.00));
+		list.add(new Produto("Notebook", 20.00));
+		list.add(new Produto("Tablet", 450.00));
+		list.add(new Produto("Galaxy", 75.00));
+		
+		list.removeIf(p -> p.getValor() >= 100);
+	
+		System.out.println("\n--- Usando expressao lambda inline 1 ------");
+		for (Produto p : list) {
+			System.out.println(p);
+		}
+	
+		list.add(new Produto("TV", 900.00));
+		list.add(new Produto("Notebook", 20.00));
+		list.add(new Produto("Tablet", 450.00));
+		list.add(new Produto("Galaxy", 75.00));
+	
+		list.removeIf(p -> p.getNome().toUpperCase().contains("G"));
+		
+		System.out.println("\n--- Usando expressao lambda inline 2 ------");
+		list.forEach(System.out::println);
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
