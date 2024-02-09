c664369f412ff45d66765daec6438aac21bfd521
ilsantos <igor.lourencosantos@gmail.com>
2024-02-09 14:24:43 -0300

    Usando Reference Method com a interface - Predicate.

--- src/main/java/interfaces/funcional/predicate/InterfacePredicate.java
+++ src/main/java/interfaces/funcional/predicate/InterfacePredicate.java
@@ -27,10 +27,33 @@ public class InterfacePredicate {
 			System.out.println(p);
 		}
 		
+//		===============================================================
 		
+		list.add(new Produto("TV", 900.00));
+		list.add(new Produto("Notebook", 20.00));
+		list.add(new Produto("Tablet", 450.00));
+		list.add(new Produto("Galaxy", 75.00));
 		
+		list.removeIf(Produto::staticProdutoPrecoPredicate);
 		
+		System.out.println("\n--- Usando Reference Method com metodo static 1 ------");
+		for (Produto p : list) {
+			System.out.println(p);
+		}
 		
+//		===============================================================
+		
+		list.add(new Produto("TV", 900.00));
+		list.add(new Produto("Notebook", 20.00));
+		list.add(new Produto("Tablet", 450.00));
+		list.add(new Produto("Galaxy", 75.00));
+		
+		list.removeIf(Produto::staticProdutoProdutoPredicate);
+		
+		System.out.println("\n--- Usando Reference Method com metodo static 2 ------");
+		for (Produto p : list) {
+			System.out.println(p);
+		}
 
 	}
 
--- src/main/java/interfaces/funcional/predicate/entities/Produto.java
+++ src/main/java/interfaces/funcional/predicate/entities/Produto.java
@@ -15,4 +15,13 @@ public class Produto {
 
 	private String nome;
 	private Double valor;
+	
+	public static boolean staticProdutoPrecoPredicate(Produto p) {
+		return p.getValor() >= 100.0;
+	}
+	
+	
+	public static boolean staticProdutoProdutoPredicate(Produto p) {
+		return p.getNome().toUpperCase().contains("N");
+	}
 }
