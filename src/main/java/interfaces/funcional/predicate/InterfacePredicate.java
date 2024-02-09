f7b583b824112d07723fece1b2a10697306509f0
ilsantos <igor.lourencosantos@gmail.com>
2024-02-09 14:37:12 -0300

    Usando expressao lambda declarada com a interface - Predicate.

--- src/main/java/interfaces/funcional/predicate/InterfacePredicate.java
+++ src/main/java/interfaces/funcional/predicate/InterfacePredicate.java
@@ -2,6 +2,7 @@ package interfaces.funcional.predicate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import interfaces.funcional.predicate.entities.MyPredicate;
 import interfaces.funcional.predicate.entities.Produto;
@@ -80,6 +81,38 @@ public class InterfacePredicate {
 		list.removeIf(Produto::nonStaticProdutoProdutoPredicate);
 
 		System.out.println("\n--- Usando Reference Method com metodo não static 2 ------");
+		for (Produto p : list) {
+			System.out.println(p);
+		}
+		
+//		===============================================================
+		
+		list.add(new Produto("TV", 900.00));
+		list.add(new Produto("Notebook", 20.00));
+		list.add(new Produto("Tablet", 450.00));
+		list.add(new Produto("Galaxy", 75.00));
+		
+		double min = 100.0;
+		Predicate<Produto> criteria1 = p -> p.getValor() >= min;
+
+		list.removeIf(criteria1);
+
+		System.out.println("\n--- Usando expressao lambda declarada 1 ------");
+		for (Produto p : list) {
+			System.out.println(p);
+		}
+		
+		list.add(new Produto("TV", 900.00));
+		list.add(new Produto("Notebook", 20.00));
+		list.add(new Produto("Tablet", 450.00));
+		list.add(new Produto("Galaxy", 75.00));
+		
+		String c = "N";
+		Predicate<Produto> criteria2 = p -> p.getNome().toUpperCase().contains(c);
+		
+		list.removeIf(criteria2);
+		
+		System.out.println("\n--- Usando expressao lambda declarada 2 ------");
 		for (Produto p : list) {
 			System.out.println(p);
 		}
